6ff275dc14613e50eee627e7d8defa385512bb07
summerwind <624324188@qq.com>
2018-08-15 17:29:47 +0800

    Create main.go

---
+++ main/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "path"
+    "os"
+    "fmt"
+    "github.com/lestrrat/go-file-rotatelogs"
+    "github.com/pkg/errors"
+    "github.com/rifflock/lfshook"
+    "os/exec"
+    "io/ioutil"
+    "encoding/json"
+)
+
+func ConfigLocalFileSystemLogger(logPath string, logFileName string, maxAge time.Duration, rotationTime time.Duration) {
+}
+
+func main() {
+}
